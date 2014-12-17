b04338ac7e03cbea7f7e0dc718e58183f53cc01e
amandapickering <pickering.amanda@gmail.com>
2014-12-17 12:31:24 -0500

    fixed heroku port

--- app.py
+++ app.py
@@ -52,5 +52,6 @@ def result():
 	return render_template('index.html', shuffled=new_text)
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    #app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
